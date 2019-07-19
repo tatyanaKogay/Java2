dcd9e863408672ab381a4c806d4a547b597a5526
tatyanaKogay <fisikmaster@mail.ru>
2019-07-19 09:51:31 +0600

    HW

---
+++ Java2.1(2)/src/Wall.java
@@ -0,0 +1,11 @@
+public class Wall implements Obstacle{
+    private int height;
+    public Wall(int h){
+        this.height = h;
+    }
+    public int getheight() {
+        return height;
+    }
+
+
+}
